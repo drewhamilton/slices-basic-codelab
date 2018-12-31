21969fd4eec42fd640171f15e4f639e2c52ce55b
Drew Hamilton <drew.hamilton.0@gmail.com>
2018-12-31 11:16:39 +0100

    Add MySliceProvider

---
+++ step-00/SliceCodelab/app/src/main/java/com/android/example/slicecodelab/MySliceProvider.java
@@ -0,0 +1,33 @@
+package com.android.example.slicecodelab;
+
+import android.net.Uri;
+
+import java.util.Objects;
+
+import androidx.annotation.Nullable;
+import androidx.slice.Slice;
+import androidx.slice.SliceProvider;
+
+public class MySliceProvider extends SliceProvider {
+
+    @Override
+    public boolean onCreateSliceProvider() {
+        return true;
+    }
+
+    @Override
+    public Slice onBindSlice(Uri sliceUri) {
+        switch(Objects.requireNonNull(sliceUri.getPath())) {
+            case "/temperature":
+                return createTemperatureSlice(sliceUri);
+            default:
+                return null;
+        }
+    }
+
+    @Nullable
+    private Slice createTemperatureSlice(Uri sliceUri) {
+        // TODO
+        return null;
+    }
+}
